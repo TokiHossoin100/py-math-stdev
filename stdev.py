a0f637df84f8d0f5105f20c33bf28f033f7bc56d
Morgan <41788057+fnetno@users.noreply.github.com>
2018-09-11 15:19:43 +0100

    Create stdev.py

---
+++ stdev.py
@@ -0,0 +1,17 @@
+#Import statistics module and defining variables
+import statistics as st
+dataset = []
+x = 0
+
+print("To stop entering data, enter anything other than a number.")
+while True:		#start while loop
+	try:
+        temp = float(input("Entry {}: ".format(x)))		#user input to temp
+        dataset.append(temp)	#adds temp to end of list
+        x += 1		#increments x by 1
+    except ValueError:     #catches exception when a non-float is entered (exit loop)
+        break
+
+#Calculate and print standard deviation
+sd = st.stdev(dataset)
+print("s = {}".format(sd))
