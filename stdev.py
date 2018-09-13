1382de31e9439fdc71d478ce8a1bb5cabfc3e746
Morgan <41788057+fnetno@users.noreply.github.com>
2018-09-13 14:55:13 +0100

    Update stdev.py
    Replaced spaces with tabs

--- stdev.py
+++ stdev.py
@@ -8,11 +8,11 @@ print("Please enter your data (more precise data = more precise result)")
 print("To stop entering data, enter anything other than a number.")
 while True:		#start while loop
 	try:
-        temp = float(input("Entry {}: ".format(x)))		#user input to temp
-        dataset.append(temp)	#adds temp to end of list
-        x += 1		#increments x by 1
-    except ValueError:     #catches exception when a non-float is entered (exit loop)
-        break
+		temp = float(input("Entry {}: ".format(x)))		#user input to temp
+		dataset.append(temp)	#adds temp to end of list
+		x += 1		#increments x by 1
+	except ValueError:     #catches exception when a non-float is entered (exit loop)
+		break
 
 #Return standard deviation and mean
 sd = st.stdev(dataset)
